0aca50a0d63cc4ae2d4ed81469b88cea4bb3b4ee
Carlos Henrique Lima Melara <charlesmelara@outlook.com>
2021-11-30 12:24:12 -0300

    dfs: add help to program. Prints help text with -h flag

--- allium/dfs/torrent.py
+++ allium/dfs/torrent.py
@@ -21,10 +21,26 @@ def main():
 
     if "-f" in opts:
         torrent_created = create_torrent(args, '')
+    elif "-h" in opts:
+        help()
     elif "--psize" in opts:
         tor = read_tor_file(args[0])
         psize = calculate_piece_size(tor)
         print(f'The piece size is {psize}')
+
+def help():
+    help_text = """Welcome to Allium, this is the distributed file sharing module (dfs).
+    This software is licensed with the GPL-3+, see the License file for more info.
+
+    You have quite a few options and flags to use here. Let's see a list of them:
+    -h: help
+            Print this help text
+    -f: create torrent file from files
+            Pass the files as arguments after the option to create a torrent
+            file for each one of them.
+    --psize: calculates the piece from the torrent file
+            Pass the torrent file as argument after this option."""
+    print(help_text)
 
 def calculate_piece_size(torr_file):
     return torr_file.piece_size
