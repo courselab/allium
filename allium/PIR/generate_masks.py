452ac326ea6fa5c8c04f588cf7b243a54d49b158
lucas-akira <lucas.morishita@usp.br>
2021-12-14 11:30:29 -0300

    Change mask generation to uint8, bitwise masks

--- allium/PIR/generate_masks.py
+++ allium/PIR/generate_masks.py
@@ -4,103 +4,75 @@ import numpy as np
 ### Client-side functions ###
 def createXorMasks(num_servers, database_len, data_index):
 
+    mask_len_bytes = database_len // 8
+    remainder_bits = database_len % 8
+
+    if remainder_bits > 0:
+        mask_len_bytes += 1
+        last_bits = 128
+        for i in range(remainder_bits-1):
+            last_bits = last_bits >> 1
+            last_bits += 128
+
+
     rand_masks = []
-    cumulative_xor = np.zeros((database_len,), dtype='bool')
+    cumulative_xor = np.zeros((mask_len_bytes,), dtype='uint8')
 
     # Create random bit masks
     for i in range(num_servers-1):
-        mask = np.zeros((database_len,), dtype='bool')
+        mask = np.zeros((mask_len_bytes,), dtype='uint8')
 
-        for j in range(database_len):
-            mask[j] = bool(secrets.randbits(1))
+        for j in range(mask_len_bytes-1):
+            mask[j] = secrets.randbits(8)
             cumulative_xor[j] ^= mask[j]
+
+        mask[mask_len_bytes-1] = secrets.randbits(8)
+        mask[mask_len_bytes-1] &= last_bits
+        cumulative_xor[j] ^= mask[mask_len_bytes-1]
 
         rand_masks.append(mask)
 
-    index_array = np.zeros((database_len,), dtype='bool')
-    index_array[data_index] = True
+    # Debug
+    # i = 0
+    # for mask in rand_masks:
+    #     print("Mask", i, ":")
+    #     for j in range(mask_len_bytes):
+    #         print("  byte", j, ":", "{:08b}".format(mask[j]))
+    #     i += 1
 
-    # Create last mask
-    last_mask = np.zeros((database_len,), dtype='bool')
-    for j in range(database_len):
+    # Create index array i.e. the request
+    index_array = np.zeros((mask_len_bytes,), dtype='uint8')
+    byte_index  = data_index // 8
+    bit_index   = 7 - (data_index % 8)
+    index_array[byte_index] = 2**bit_index
+
+    # Debug
+    # print("Index array:")
+    # for j in range(mask_len_bytes):
+    #     print("  byte", j, ":", "{:08b}".format(index_array[j]))
+
+    #Create last mask
+    last_mask = np.zeros((mask_len_bytes,), dtype='uint8')
+    for j in range(mask_len_bytes):
         last_mask[j] = (index_array[j] ^ cumulative_xor[j])
 
-    # verify = np.zeros((database_len,), dtype='bool')
-    # for j in range(database_len):
-    #     verify[j] = (last_mask[j] ^ cumulative_xor[j])
-    # print("Verification: ", verify)
+    # Debug
+    # print("Last mask:")
+    # for j in range(mask_len_bytes):
+    #     print("  byte", j, ":", "{:08b}".format(last_mask[j]))
 
+    # Debug
+    # verify = np.zeros((mask_len_bytes,), dtype='uint8')
+    # for j in range(mask_len_bytes):
+    #     verify[j] = (last_mask[j] ^ cumulative_xor[j])
+
+    # Debug
+    # print("Verification:")
+    # for j in range(mask_len_bytes):
+    #     print("  byte", j, ":", "{:08b}".format(verify[j]))
 
     return rand_masks, last_mask
 
 # masks, last_mask = createXorMasks(3, 10, 2)
 # print(masks)
 # print(last_mask)
-
-### Server-side fucntions ###
-database_len = 10
-word_size_bits = 8
-index = 7
-
-database = np.zeros((database_len, word_size_bits), dtype='bool')
-database[0,:] = np.array([False, False, True, False, True, True, True, False])
-database[1,:] = np.array([False, False, True, False, True, False, False, False])
-database[2,:] = np.array([True, True, False, True, True, False, False, False])
-database[3,:] = np.array([False, True, True, True, True, True, False, False])
-database[4,:] = np.array([False, False, False, True, False, True, False, False])
-database[5,:] = np.array([True, True, False, True, False, True, True, True])
-database[6,:] = np.array([True, False, False, True, True, False, True, True])
-database[7,:] = np.array([True, True, True, True, False, True, True, True])
-database[8,:] = np.array([False, False, False, False, False, True, False, True])
-database[9,:] = np.array([False, False, False, False, True, True, False, False])
-
-def processDBrequest(index_array):
-    # print(database)
-    # print(index_array)
-    response = np.matmul(index_array.astype(np.int), database.astype(np.int))
-    response %= 2
-    response = np.array(response, dtype='bool')
-    return response
-
-
-# response = processDBrequest(np.array([False, False, True, False, False, True, True, False, False, True]))
-# print(response)
-
-### Testing ###
-print("Database:")
-for j in range(database_len):
-    print(" Index", j, ":", database[j,:].astype(np.int))
-
-print("------------------------")
-
-masks, last_mask = createXorMasks(num_servers=3, database_len=10, data_index=index)
-print("Random masks generated:")
-for mask in masks:
-    print(mask.astype(np.int))
-
-print("Last mask:")
-print(last_mask.astype(np.int))
-
-
-server_responses = []
-data_requested = np.zeros((word_size_bits,), dtype='bool')
-
-for rand_mask in masks:
-    response = processDBrequest(rand_mask)
-    server_responses.append(response)
-    data_requested ^= response
-    # for j in range(word_size_bits):
-    #     data_requested[j] ^= response[j]
-
-response = processDBrequest(last_mask)
-server_responses.append(response)
-data_requested ^= response
-# for j in range(word_size_bits):
-#     data_requested[j] ^= response[j]
-
-print("Server responses:")
-for response in server_responses:
-    print(response.astype(np.int))
-
-print("Data requested at index", index, "is:")
-print(data_requested.astype(np.int))
